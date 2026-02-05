819720d5a9361929eb37ec5d76345d102b6cd9bb
Eliezer Yakovson <eliezeryakovson@gmail.com>
2026-02-05 12:24:26 +0200

    Configuring data upload from the JSON file in the connection file

--- app/connection.py
+++ app/connection.py
@@ -1,8 +1,16 @@
 from pymongo import MongoClient
 import os
+import json
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+file_path = './employee_data_advanced.json'
 
 client = MongoClient(MONGO_URI)
 db = client["employee_db"]
 employees_collection = db["employees"]
+
+with open(file_path) as file:
+    file_data = json.load(file)
+
+ins_result = employees_collection.insert_many(file_data)
+print(f"Data inserted to MongoDB. Documents inserted: {len(ins_result.inserted_ids)}")
